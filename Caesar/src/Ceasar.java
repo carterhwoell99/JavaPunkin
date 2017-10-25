9b2bd2e4a89cb37d776e884a40ae015c4beb14df
carterhwoell99 <carterhowell9011@gmail.com>
2017-10-25 10:02:00 -0600

    The beginning of Caesar Cipher.
    A template for my Caesar Cipher project.

---
+++ Caesar/src/Ceasar.java
@@ -0,0 +1,29 @@
+
+public class Ceasar {
+
+	public static void main(String[] args) {
+		Cipher myCipher = new Cipher();
+		myCipher.encode("dog", 1);
+		
+		// Create an instance of a class called Cipher
+
+	}
+
+}
+
+class Cipher {
+	public Cipher() {
+		
+	}
+	String encode(String plainText, int key) {
+		String alpha = "abcdefghijklmnopqrstuvwxyz";
+		plainText = "dog";
+		while () {
+			
+		}
+		return("");
+		
+	}
+	
+	
+}
